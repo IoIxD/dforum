b51eeb5ee463c81947cf72d709f224e15adf2e25
Samuel Hernandez <sam@samhza.com>
2022-09-16 02:28:03 -0400

    clean up templates, use go-chi, discord markdown, rewrite sitemap

--- funcmap.go
+++ funcmap.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"html/template"
-
-	"github.com/disgoorg/snowflake/v2"
+	"fmt"
+	"time"
 )
 
-func FuncMap(b *Bot) template.FuncMap {
-	return template.FuncMap{
-		"GetAvatarURL":         b.GetAvatarURL,
-		"GetForums":            b.GetForums,
-		"GetGuildName":         b.GetGuildName,
-		"GetThreadsInChannel":  b.GetThreadsInChannel,
-		"GetChannelTitle":      b.GetChannelTitle,
-		"GetMessagesInChannel": b.GetMessagesInChannel,
-		"PostCount":            b.PostCount,
-		"PrettyTime":           PrettyTime,
-		"FormatDiscordThings":  b.FormatDiscordThings,
-		"GuildNum":             b.GuildNum,
-		"ParseSnowflake":       snowflake.MustParse,
+var funcMap = map[string]any{
+	"PrettyTime": PrettyTime,
+}
+
+func PrettyTime(timestamp time.Time) string {
+	unixTimeDur := time.Now().Sub(timestamp)
+
+	if unixTimeDur.Hours() >= 8760 {
+		return fmt.Sprintf("%0.f years ago", unixTimeDur.Hours()/8760)
 	}
+	if unixTimeDur.Hours() >= 730 {
+		return fmt.Sprintf("%0.f months ago", unixTimeDur.Hours()/730)
+	}
+	if unixTimeDur.Hours() >= 168 {
+		return fmt.Sprintf("%0.f weeks ago", unixTimeDur.Hours()/168)
+	}
+	if unixTimeDur.Hours() >= 24 {
+		return fmt.Sprintf("%0.f days ago", unixTimeDur.Hours()/24)
+	}
+	if unixTimeDur.Hours() >= 1 {
+		return fmt.Sprintf("%0.f hours ago", unixTimeDur.Hours())
+	}
+	if unixTimeDur.Minutes() >= 1 {
+		return fmt.Sprintf("%0.f minutes ago", unixTimeDur.Minutes())
+	}
+	return fmt.Sprintf("%0.f seconds ago", unixTimeDur.Seconds())
 }
