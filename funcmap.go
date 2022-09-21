026fd09a8ac0916b4af52c6414e8606bd7c9ff27
Samuel Hernandez <sam@samhza.com>
2022-09-20 23:50:02 -0400

    better timestamps, remove attachments <p>

--- funcmap.go
+++ funcmap.go
@@ -6,29 +6,41 @@ import (
 )
 
 var funcMap = map[string]any{
-	"PrettyTime": PrettyTime,
+	"PrettyTimeSince": PrettyTimeSince,
 }
 
-func PrettyTime(timestamp time.Time) string {
-	unixTimeDur := time.Now().Sub(timestamp)
-
-	if unixTimeDur.Hours() >= 8760 {
-		return fmt.Sprintf("%0.f years ago", unixTimeDur.Hours()/8760)
+func PrettyTimeSince(timestamp time.Time) string {
+	dur := time.Since(timestamp)
+	switch {
+	case dur < time.Second:
+		return "just now"
+	case dur < time.Second*2:
+		return "1 second ago"
+	case dur < time.Minute:
+		return fmt.Sprintf("%d seconds ago", dur/time.Second)
+	case dur < time.Minute*2:
+		return "1 minute ago"
+	case dur < time.Hour:
+		return fmt.Sprintf("%d minutes ago", dur/time.Minute)
+	case dur < time.Hour*2:
+		return "1 hour ago"
+	case dur < time.Hour*24:
+		return fmt.Sprintf("%d hours ago", dur/time.Hour)
+	case dur < time.Hour*24*2:
+		return "1 day ago"
+	case dur < time.Hour*24*7:
+		return fmt.Sprintf("%d days ago", dur/(time.Hour*24))
+	case dur < time.Hour*24*7*2:
+		return "1 week ago"
+	case dur < time.Hour*24*7*4:
+		return fmt.Sprintf("%d weeks ago", dur/(time.Hour*24*7))
+	case dur < time.Hour*24*7*4*2:
+		return "1 month ago"
+	case dur < time.Hour*24*7*4*12:
+		return fmt.Sprintf("%d months ago", dur/(time.Hour*24*7*4))
+	case dur < time.Hour*24*7*4*12*2:
+		return "1 year ago"
+	default:
+		return fmt.Sprintf("%d years ago", dur/(time.Hour*24*7*4*12))
 	}
-	if unixTimeDur.Hours() >= 730 {
-		return fmt.Sprintf("%0.f months ago", unixTimeDur.Hours()/730)
-	}
-	if unixTimeDur.Hours() >= 168 {
-		return fmt.Sprintf("%0.f weeks ago", unixTimeDur.Hours()/168)
-	}
-	if unixTimeDur.Hours() >= 24 {
-		return fmt.Sprintf("%0.f days ago", unixTimeDur.Hours()/24)
-	}
-	if unixTimeDur.Hours() >= 1 {
-		return fmt.Sprintf("%0.f hours ago", unixTimeDur.Hours())
-	}
-	if unixTimeDur.Minutes() >= 1 {
-		return fmt.Sprintf("%0.f minutes ago", unixTimeDur.Minutes())
-	}
-	return fmt.Sprintf("%0.f seconds ago", unixTimeDur.Seconds())
 }
