cebacee9c041784e42e7b677b3466ef820801d6b
IoIxD <alphaproject217@gmail.com>
2022-09-28 16:27:27 -0700

    slight code cleanup

--- imageproxy.go
+++ imageproxy.go
@@ -2,103 +2,69 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"image"
-	"image/gif"
 	"image/jpeg"
 	"image/png"
+	"io"
 	"net/http"
 	"regexp"
 
-	"github.com/Kagami/go-avif"
 	"github.com/chai2010/webp"
 )
 
 var extensionRegexp = regexp.MustCompile(`\.(png|jpeg|jpg|webp|gif)`)
 
 func (s *server) ProxyPage(w http.ResponseWriter, r *http.Request) {
-	// forcefully branch into a thread
-	ready := make(chan int, 1)
-	var finalImage bytes.Buffer
-	var format, url string
-	var err error
+	// the url to encode
+	url := r.URL.Query().Get("url")
+	if url == "" {
+		return
+	}
+	// the contents of the url
+	resp, err := http.Get(url)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Header().Add("Content-Name", "error.html")
+		w.Write([]byte(err.Error()))
+		return
+	}
+	// the file extension
+	ext := string(extensionRegexp.Find([]byte(url)))
 
-	go func(ready chan int, finalImage *bytes.Buffer) {
-		// the format to encode to
-		format = r.URL.Query().Get("format")
-		if format == "" || (format != "webp" && format != "avif") {
-			format = "webp"
-		}
+	var image image.Image
 
-		// the url to encode
-		url := r.URL.Query().Get("url")
-		if url == "" {
-			return
-		}
-		var resp *http.Response
-
-		// the contents of the url
-		resp, err = http.Get(url + "?size=256")
-		if err != nil {
-			ready <- 1
-			return
-		}
-
-		// the file extension
-		ext := string(extensionRegexp.Find([]byte(url)))
-
-		var image image.Image
-
-		switch ext {
-		case ".png":
-			image, err = png.Decode(resp.Body)
-		case ".jpeg", ".jpg":
-			image, err = jpeg.Decode(resp.Body)
-		case ".gif":
-			image, err = gif.Decode(resp.Body)
-		case ".webp":
-			if format == "webp" {
-				w.Header().Set("Content-Type", "image/webp")
-				w.Header().Set("Content-Disposition", "attachment; filename="+url)
-				w.WriteHeader(200)
-				var fuck []byte
-				_, _ = resp.Body.Read(fuck)
-				w.Write(fuck)
-			} else {
-				image, err = webp.Decode(resp.Body)
-			}
-		}
-
-		if err != nil {
-			ready <- 1
-			return
-		}
-
-		switch format {
-		case "webp":
-			err = webp.Encode(finalImage, image, &webp.Options{
-				Quality: 25,
-			})
-		case "avif":
-			err = avif.Encode(finalImage, image, &avif.Options{
-				Threads: 2,
-				Quality: 63,
-			})
-		}
-		ready <- 1
-	}(ready, &finalImage)
-	_ = <-ready
+	switch ext {
+	case ".png":
+		image, err = png.Decode(resp.Body)
+	case ".jpeg", ".jpg":
+		image, err = jpeg.Decode(resp.Body)
+	case ".gif", ".webp":
+		w.Header().Set("Content-Type", "image/"+ext)
+		w.Header().Set("Content-Disposition", "attachment; filename="+url)
+		w.WriteHeader(200)
+		io.Copy(w, resp.Body)
+	}
 
 	if err != nil {
 		w.WriteHeader(500)
-		w.Header().Set("Content-Type", "text/html")
 		w.Header().Add("Content-Name", "error.html")
 		w.Write([]byte(err.Error()))
 		return
 	}
 
-	w.Header().Set("Content-Type", "image/"+format)
-	w.Header().Set("Content-Disposition", "attachment; filename="+url+format)
+	var webpImage bytes.Buffer
+	err = webp.Encode(&webpImage, image, &webp.Options{
+		Quality: 25,
+	})
+	if err != nil {
+		w.WriteHeader(500)
+		w.Header().Add("Content-Name", "error.html")
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/webp")
+	w.Header().Set("Content-Disposition", "attachment; filename="+url+".webp")
 	w.WriteHeader(200)
-	w.Write(finalImage.Bytes())
+	w.Write(webpImage.Bytes())
 }
