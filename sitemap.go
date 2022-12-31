81498946ddade26cc44d9a98fcaffeebd0a184b6
IoIxD <alphaproject217@gmail.com>
2022-12-31 01:50:57 -0700

    sitemap chunking

--- sitemap.go
+++ sitemap.go
@@ -6,13 +6,24 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/diamondburned/arikawa/v3/discord"
 )
 
+const LIMIT = 250
+
 type URL struct {
 	XMLName   string  `xml:"url"`
+	Location  string  `xml:"loc"`
+	LastMod   string  `xml:"lastmod,omitempty"`
+	Frequency string  `xml:"changefreq,omitempty"`
+	Priority  float32 `xml:"priority,omitempty"`
+}
+
+type SitemapEntry struct {
+	XMLName   string  `xml:"sitemap"`
 	Location  string  `xml:"loc"`
 	LastMod   string  `xml:"lastmod,omitempty"`
 	Frequency string  `xml:"changefreq,omitempty"`
@@ -23,9 +34,10 @@ func (s *server) getSitemap(w http.ResponseWriter, r *http.Request) {
 	s.sitemapMu.Lock()
 	var sitemap []byte
 	var modtime time.Time
-	if s.sitemap == nil || time.Since(s.sitemapUpdated) > 6*time.Hour {
+	offset := r.URL.Query().Get("offset")
+	if s.sitemaps[offset] == nil || time.Since(s.sitemapUpdated) > 6*time.Hour {
 		buf := s.buffers.Get().(*bytes.Buffer)
-		err := s.writeSitemap(buf)
+		err := s.writeSitemap(buf, offset)
 		if err != nil {
 			s.sitemapMu.Unlock()
 			buf.Reset()
@@ -35,14 +47,14 @@ func (s *server) getSitemap(w http.ResponseWriter, r *http.Request) {
 		}
 		sitemap = make([]byte, buf.Len())
 		copy(sitemap, buf.Bytes())
-		s.sitemap = sitemap
+		s.sitemaps[offset] = sitemap
 		s.sitemapUpdated = time.Now()
 		modtime = s.sitemapUpdated
 		s.sitemapMu.Unlock()
 		buf.Reset()
 		s.buffers.Put(buf)
 	} else {
-		sitemap = s.sitemap
+		sitemap = s.sitemaps[offset]
 		modtime = s.sitemapUpdated
 		s.sitemapMu.Unlock()
 	}
@@ -57,25 +69,42 @@ var XMLURLSetStart = xml.StartElement{
 	},
 }
 
-var XMLURLSetEnd = XMLURLSetStart.End()
+var SitemapSetStart = xml.StartElement{
+	Name: xml.Name{Local: "sitemapindex"},
+	Attr: []xml.Attr{
+		{Name: xml.Name{Local: "xmlns"}, Value: "http://www.sitemaps.org/schemas/sitemap/0.9"},
+	},
+}
 
-func (s *server) writeSitemap(w io.Writer) error {
+var XMLURLSetEnd = XMLURLSetStart.End()
+var SitemapSetEnd = SitemapSetStart.End()
+
+var guildList []URL = make([]URL, 0)
+var chanList []URL = make([]URL, 0)
+var postList []URL = make([]URL, 0)
+
+func (s *server) writeSitemap(w io.Writer, offset string) error {
 	if _, err := io.WriteString(w, xml.Header); err != nil {
 		return err
 	}
 	enc := xml.NewEncoder(w)
 	var err error
-	if err = enc.EncodeToken(XMLURLSetStart); err != nil {
-		return err
+	if offset == "" {
+		if err = enc.EncodeToken(SitemapSetStart); err != nil {
+			return err
+		}
+	} else {
+		if err = enc.EncodeToken(XMLURLSetStart); err != nil {
+			return err
+		}
 	}
+
 	guilds, _ := s.discord.Cabinet.Guilds()
 	me, _ := s.discord.Cabinet.Me()
 	for _, guild := range guilds {
-		if err = enc.Encode(URL{
+		guildList = append(guildList, URL{
 			Location: fmt.Sprintf("%s/%s", s.URL, guild.ID),
-		}); err != nil {
-			return err
-		}
+		})
 		memberSelf, err := s.discord.Member(guild.ID, me.ID)
 		if err != nil {
 			return fmt.Errorf("error fetching self as member: %w", err)
@@ -94,27 +123,82 @@ func (s *server) writeSitemap(w io.Writer) error {
 				discord.PermissionViewChannel) {
 				continue
 			}
-			if err = enc.Encode(URL{
+			chanList = append(chanList, URL{
 				Location: fmt.Sprintf("%s/%s/%s", s.URL, guild.ID, forum.ID),
-			}); err != nil {
-				return err
-			}
+			})
 			for _, post := range channels {
 				if post.ParentID != forum.ID ||
 					post.Type != discord.GuildPublicThread {
 					continue
 				}
-				if err = enc.Encode(URL{
+				postList = append(postList, URL{
 					Location: fmt.Sprintf("%s/%s/%s/%s", s.URL, guild.ID, forum.ID, post.ID),
-				}); err != nil {
+				})
+			}
+		}
+	}
+
+	if offset == "" {
+		// put a url for every possible "offset url" there might be
+		lenAll := len(guildList) + len(chanList) + len(postList)
+		parts := lenAll % LIMIT
+		for i := 0; i <= parts; i++ {
+			if err = enc.Encode(SitemapEntry{
+				Location: fmt.Sprintf("%s/sitemap.xml?offset=%d", s.URL, i*LIMIT),
+			}); err != nil {
+				return err
+			}
+		}
+		if err = enc.EncodeToken(SitemapSetEnd); err != nil {
+			return err
+		}
+	} else {
+		offset, err := strconv.Atoi(offset)
+		if err != nil {
+			return err
+		}
+
+		for i := 0; i <= len(guildList); i++ {
+			if i < offset {
+				continue
+			}
+			if i > offset+LIMIT {
+				break
+			} else {
+				if err = enc.Encode(guildList[i]); err != nil {
 					return err
 				}
 			}
 		}
+		for i := 0; i <= len(chanList); i++ {
+			if i < offset {
+				continue
+			}
+			if i > offset+LIMIT {
+				break
+			} else {
+				if err = enc.Encode(chanList[i]); err != nil {
+					return err
+				}
+			}
+		}
+		for i := 0; i <= len(postList); i++ {
+			if i < offset {
+				continue
+			}
+			if i > offset+LIMIT {
+				break
+			} else {
+				if err = enc.Encode(postList[i]); err != nil {
+					return err
+				}
+			}
+		}
+		if err = enc.EncodeToken(XMLURLSetEnd); err != nil {
+			return err
+		}
 	}
-	if err = enc.EncodeToken(XMLURLSetEnd); err != nil {
-		return err
-	}
+
 	if err = enc.Flush(); err != nil {
 		return err
 	}
