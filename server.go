8c54e37674bbcec9a10d69775872dc76aabf3139
samhza <sam@samhza.com>
2023-04-20 23:22:40 -0400

    Revert chunk (#95)
    * fix sitemap chunking
    
    resolves #53
    
    * add database option to config.example.toml
    
    * fix post sort on forum view
    
    fixes #85
    
    * don't display broken image for guilds without icons
    
    * rewrite .github/pull_request_template.md

--- server.go
+++ server.go
@@ -8,7 +8,10 @@ import (
 	"hash/crc32"
 	"io"
 	"io/fs"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"sort"
 	"strconv"
 	"sync"
@@ -35,14 +38,14 @@ type server struct {
 	requestMembers sync.Mutex
 	membersGot     map[discord.ChannelID]struct{}
 
-	sitemaps       map[string][]byte
-	sitemapUpdated time.Time
-	sitemapMu      sync.Mutex
+	sitemapMu     sync.Mutex
+	updateSitemap chan struct{}
 
 	// configuration options
 	URL               string
 	ServiceName       string
 	ServerHostedIn    string
+	SitemapDir        string
 	executeTemplateFn ExecuteTemplateFunc
 
 	buffers *sync.Pool
@@ -59,6 +62,7 @@ func newServer(st *state.State, fsys fs.FS, db database.Database, config config)
 		URL:             config.SiteURL,
 		ServiceName:     config.ServiceName,
 		ServerHostedIn:  config.ServerHostedIn,
+		SitemapDir:      config.SitemapDir,
 	}
 	st.AddHandler(func(m *gateway.MessageCreateEvent) {
 		srv.messageCache.Set(context.Background(), m.Message, false)
@@ -74,8 +78,9 @@ func newServer(st *state.State, fsys fs.FS, db database.Database, config config)
 	})
 	r := chi.NewRouter()
 	srv.r = r
-	srv.sitemaps = make(map[string][]byte)
+	srv.updateSitemap = make(chan struct{}, 1)
 	r.Use(middleware.Logger)
+	getHead(r, `/sitemap/*`, srv.getSitemap)
 	getHead(r, `/sitemap.xml`, srv.getSitemap)
 	getHead(r, "/", srv.getIndex)
 	r.Route("/{guildID:\\d+}", func(r chi.Router) {
@@ -95,6 +100,36 @@ func newServer(st *state.State, fsys fs.FS, db database.Database, config config)
 		srv.displayErr(w, http.StatusNotFound, nil)
 	}))
 	return srv, nil
+}
+
+func (s *server) UpdateSitemap() {
+	first := true
+	ticker := time.NewTicker(6 * time.Hour)
+	for {
+		index := filepath.Join(s.SitemapDir, "sitemap.xml")
+		stat, err := os.Stat(index)
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			log.Println("Error occured while reading sitemap last modified time:", err)
+			continue
+		}
+		if err == nil && time.Since(stat.ModTime()) < 6*time.Hour {
+			continue
+		} else {
+			if first {
+				log.Println("Waiting 60 seconds before generating sitemap.")
+				time.Sleep(60 * time.Second)
+				first = false
+			}
+			err = s.writeSitemap()
+			if err != nil {
+				log.Println("Error occured while writing sitemap:", err)
+			}
+		}
+		select {
+		case <-ticker.C:
+		case <-s.updateSitemap:
+		}
+	}
 }
 
 func getHead(r chi.Router, path string, handler http.HandlerFunc) {
@@ -289,11 +324,11 @@ func (s *server) getForum(w http.ResponseWriter, r *http.Request) {
 		}
 		posts = append(posts, post)
 	}
-	sort.SliceStable(ctx.Posts, func(i, j int) bool {
-		if ctx.Posts[i].Flags^ctx.Posts[j].Flags&discord.PinnedThread != 0 {
-			return ctx.Posts[i].Flags&discord.PinnedThread != 0
+	sort.SliceStable(posts, func(i, j int) bool {
+		if posts[i].Flags^posts[j].Flags&discord.PinnedThread != 0 {
+			return posts[i].Flags&discord.PinnedThread != 0
 		}
-		return ctx.Posts[i].LastMessageID.Time().After(ctx.Posts[j].LastMessageID.Time())
+		return posts[i].LastMessageID.Time().After(posts[j].LastMessageID.Time())
 	})
 	page, err := strconv.Atoi(chi.URLParam(r, "page"))
 	if err != nil || page < 1 {
